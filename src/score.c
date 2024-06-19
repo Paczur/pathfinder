8eadb9c83adb9c18d69d3ef8dd2f1df04e4773e4
Paczur <149335135+Paczur@users.noreply.github.com>
2024-06-19 19:02:02 +0200

    Fix score for only one node

--- src/score.c
+++ src/score.c
@@ -1,10 +1,12 @@
 #include "score.h"
 #include <stdio.h>
 
-#define SCORE_INIT(field)                                                   \
-  uint field =                                                              \
-    stats->field[0] * SCORE_PRECISION / (SCORE_LOSS * (stats->count - 1)) + \
-    stats->field[stats->count - 1] * SCORE_PRECISION
+#define SCORE_INIT(field)                                             \
+  uint field =                                                        \
+    stats->field[0] * SCORE_PRECISION /                               \
+      ((stats->count == 1) ? 1 : (SCORE_LOSS * (stats->count - 1))) + \
+    ((stats->count == 1) ? 0                                          \
+                         : stats->field[stats->count - 1] * SCORE_PRECISION)
 #define SCORE_ADD(field, i) \
   field +=                  \
     stats->field[i] * SCORE_PRECISION / (SCORE_LOSS * (stats->count - 1 - i))
@@ -16,12 +18,14 @@ uint score(const stats_t *stats) {
   SCORE_INIT(word_start);
   SCORE_INIT(word_end);
   SCORE_INIT(bad_case);
-  for(size_t i = stats->count - 2; i > 0; i--) {
-    SCORE_ADD(dirname_start, i);
-    SCORE_ADD(dirname_end, i);
-    SCORE_ADD(word_start, i);
-    SCORE_ADD(word_end, i);
-    SCORE_ADD(bad_case, i);
+  if(stats->count >= 2) {
+    for(size_t i = stats->count - 2; i > 0; i--) {
+      SCORE_ADD(dirname_start, i);
+      SCORE_ADD(dirname_end, i);
+      SCORE_ADD(word_start, i);
+      SCORE_ADD(word_end, i);
+      SCORE_ADD(bad_case, i);
+    }
   }
   depth *= SCORE_DEPTH;
   dirname_start *= SCORE_DIRNAME_START;
--- src/score.test.c
+++ src/score.test.c
@@ -13,9 +13,22 @@ TEST(score, simple) {
   assert_int_equal(ans, 83750);
 }
 
+TEST(score, one) {
+  stats_t stats = {.depth = 1,
+                   .count = 1,
+                   .dirname_start = &(uchar[]){0},
+                   .dirname_end = &(uchar[]){1},
+                   .word_start = &(uchar[]){0},
+                   .word_end = &(uchar[]){1},
+                   .bad_case = &(uchar[]){1}};
+  int ans = score(&stats);
+  assert_int_equal(ans, 91000);
+}
+
 int main(void) {
   const struct CMUnitTest tests[] = {
     ADD(score, simple),
+    ADD(score, one),
   };
   return cmocka_run_group_tests(tests, NULL, NULL);
 }
